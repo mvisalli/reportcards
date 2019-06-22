14b6d2f9c1c079bee493df6f192a3cc334a02010
mvisalli <mvisalli@ucsb.edu>
2019-06-21 21:53:03 -0700

    add second map with all AIS data to look for gaps

--- R/carnivalimagination.R
+++ R/carnivalimagination.R
@@ -1,4 +1,4 @@
-install.packages('devtools')
+#install.packages('devtools')
 library(tidyverse)
 library(devtools)
 #devtools::install_github("mvisalli/shipr")
@@ -12,7 +12,6 @@ library(readr)
 
 sbais_ci<-readRDS("data/sbais_ci.RDS") %>% 
   arrange(datetime) %>%
-  filter(!duplicated(round_date(datetime, unit="minute"))) %>% 
   st_as_sf(coords=c("lon","lat"), crs = 4326)
 
 bins_ci <- c(0,10,11,12,15,20)
@@ -29,3 +28,28 @@ map_ci <- leaflet() %>%
                    label = ~sprintf("%f knots - %s",speed,datetime),
                    color=~pal_ci(speed))
 map_ci
+
+
+
+
+
+sbais062019<-readRDS("data/sbais062019.RDS") %>% 
+  filter(lon >= -118.795374 & lon <= -117.707252,
+         lat >= 33.080975 & lat <= 33.680974)%>%
+  arrange(datetime) %>%
+  st_as_sf(coords=c("lon","lat"), crs = 4326)
+
+bins_ci <- c(0,10,11,12,15,20)
+
+pal_sbais062019 <- colorBin( "YlOrRd",
+                    domain= sbais062019$speed,
+                    bins = bins_ci,
+                    na.color = 'black')
+
+map_sbais062019 <- leaflet() %>%
+  addTiles() %>%
+  addCircleMarkers(data=sbais062019,
+                   radius = 0.5,
+                   label = ~sprintf("%f knots - %s %s",speed,datetime,name),
+                   color=~pal_sbais062019(speed))
+map_sbais062019
--- data-raw/sbais.R
+++ data-raw/sbais.R
@@ -12,3 +12,15 @@ sbais_ci <- tibble::tibble(
 
 readr::write_csv(sbais_ci, "data-raw/sbais_ci.csv")
 saveRDS(sbais_ci, "data/sbais_ci.RDS")
+
+sbais062019 <- tibble::tibble(
+  txt = list.files("data-raw", "^AIS_.*\\.txt$", full.names = T, recursive = T)) %>%
+  dplyr::mutate(
+    # note use of custom read_ais_txt() function applied to each *.txt file
+    data = purrr::map(txt, shipr::read_ais_txt)) %>%
+  dplyr::select(-txt) %>%
+  tidyr::unnest(data) %>%
+  dplyr::arrange(datetime, name)
+
+readr::write_csv(sbais062019, "data-raw/sbais062019.csv")
+saveRDS(sbais062019, "data/sbais062019.RDS")
